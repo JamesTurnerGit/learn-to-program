bd42d82649c31a4bc45ef123a7c70a9dd239c1a5
James Turner <mail.jt@gmail.com>
2016-11-24 13:10:14 +0000

    Updated exercise with better version i wrote earlier

--- chap08/ex02.rb
+++ chap08/ex02.rb
@@ -1,83 +1,51 @@
-def englishNumber number
-  if number < 0  # No negative numbers.
-    return 'Please enter a number that isn\'t negative.'
-  end
-  if number == 0
+#improvements
+#add "and" in the correct places (when you figure out where that is)
+
+def englishNumber remaining_number
+  if remaining_number == 0
     return 'zero'
   end
-
-  # No more special cases! No more returns!
-
-  numString = ''  # This is the string we will return.
-
+  number_string = remaining_number < 0 ? "negative " : ""
+  remaining_number = remaining_number.abs
+  
+  #build arrays
   onesPlace = ['one',   'two',    'three',    'four',     'five',    'six',     'seven',     'eight',    'nine']
   tensPlace = ['ten',   'twenty', 'thirty',   'forty',    'fifty',   'sixty',   'seventy',   'eighty',   'ninety']
   teenagers = ['eleven','twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-  
-  # "left" is how much of the number we still have left to write out.
-  # "write" is the part we are writing out right now.
-  # write and left... get it?  :)
-  left  = number
-  write = left / 1_000_000          
-  left  = left - write * 1_000_000  
-
-  if write > 0
-    numString = numString + englishNumber(write) + ' million'
-    if left > 0
-      numString = numString + ' '
-    end
-   end
-  
-  write = left/1000          
-  left  = left - write*1000 
-
-  if write > 0
-    numString = numString + englishNumber(write) + ' thousand'
-    if left > 0
-      numString = numString + ' '
-    end
-   end
-    
-  write = left/100          # How many hundreds left to write out?
-  left  = left - write*100  # Subtract off those hundreds.
-  if write > 0
-    numString = numString + onesPlace[write-1].to_s + ' hundred'
-    if left > 0
-      numString = numString + ' '
-    end
+  million_billion =  [[100,"hundred"]]
+  big_array = ["thousand","million","billion","trillion","quadrillion","quintillion","sextillion","septillion","octillion","nonillion","decillion","undecillion","duodecillion","tredecillion","quattuordecillion","quindecillion","sexdecillion","septendecillion","octodecillion","novemdecillion","vigintillion","unvigintillion","duovigintillion","trevigintillion","quattuorvigintillion","quinvigintillion","sexvigintillion","septenvigintillion","octovigintillion","novemvigintillion","trigintillion","untrigintillion","duotrigintillion","tretrigintillion","quattuortrigintillion","quintrigintillion","sextrigintillion","septentrigintillion","octotrigintillion","novemtrigintillion","quadragintillion","unquadragintillion","duoquadragintillion","trequadragintillion","quattuorquadragintillion","quinquadragintillion","sexquadragintillion","septenquadragintillion","octoquadragintillion","novemquadragintillion","quinquagintillion","unquinquagintillion","duoquinquagintillion","trequinquagintillion","quattuorquinquagintillion","quinquinquagintillion","sexquinquagintillion","septenquinquagintillion","octoquinquagintillion","novemquinquagintillion","sexagintillion","unsexagintillion","duosexagintillion","tresexagintillion","quattuorsexagintillion","quinsexagintillion","sexsexagintillion","septensexagintillion","octosexagintillion","novemsexagintillion","septuagintillion","unseptuagintillion","duoseptuagintillion","treseptuagintillion","quattuorseptuagintillion","quinseptuagintillion","sexseptuagintillion","septenseptuagintillion","octoseptuagintillion","novemseptuagintillion","octogintillion","unoctogintillion","duooctogintillion","treoctogintillion","quattuoroctogintillion","quinoctogintillion","sexoctogintillion","septenoctogintillion","octooctogintillion","novemoctogintillion","nonagintillion","unnonagintillion","duononagintillion","trenonagintillion","quattuornonagintillion","quinnonagintillion","sexnonagintillion","septennonagintillion","octononagintillion","novemnonagintillion","centillion"]
+  big_array.each_with_index do |array_words,index|
+    million_billion << [1000 ** (index + 1),array_words]
   end
 
-  write = left/10          # How many tens left to write out?
-  left  = left - write*10  # Subtract off those tens.
-  if write > 0
-    if ((write == 1) and (left > 0))
-      # Since we can't write "tenty-two" instead of "twelve",
-      # we have to make a special exception for these.
-      numString = numString + teenagers[left-1]
-      # The "-1" is because teenagers[3] is 'fourteen', not 'thirteen'.
-
-      # Since we took care of the digit in the ones place already,
-      # we have nothing left to write.
-      left = 0
+  #bignums 
+  million_billion.reverse_each do |array_number,array_words|
+    currently_writing = remaining_number/array_number        
+    remaining_number  -= currently_writing * array_number  
+    if currently_writing > 0
+      number_string += englishNumber(currently_writing) + ' ' + array_words
+      if remaining_number > 0
+        number_string += ' '
+      end
+    end
+  end
+  #tens
+  currently_writing = remaining_number/10
+  remaining_number  -= currently_writing*10
+  if currently_writing > 0
+    if ((currently_writing == 1) and (remaining_number > 0))
+      number_string += teenagers [remaining_number-1]
+      remaining_number = 0
     else
-      numString = numString + tensPlace[write-1]
-      # The "-1" is because tensPlace[3] is 'forty', not 'thirty'.
+      number_string += tensPlace[currently_writing-1]
     end
-
-    if left > 0
-      # So we don't write 'sixtyfour'...
-      numString = numString + '-'
+    if remaining_number > 0
+      number_string += '-'
     end
   end
-
-  write = left  # How many ones left to write out?
-  left  = 0     # Subtract off those ones.
-
-  if write > 0
-    numString = numString + onesPlace[write-1]
-    # The "-1" is because onesPlace[3] is 'four', not 'three'.
-  end
-
-  # Now we just return "numString"...
-  numString
+  #ones
+  number_string += onesPlace [remaining_number-1]  if remaining_number > 0
+  number_string
 end
+
+#puts englishNumber 5_555_555_556
